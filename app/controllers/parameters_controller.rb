62c782edd79ef5535b3e42de8647eee643e8e2b2
Grant Kazan <grantkazan@gmail.com>
2018-06-27 19:32:23 -0500

    url segments

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -1,12 +1,26 @@
 class ParametersController < ApplicationController
-  # def hello_method
-  #   @hello = "world!"
-  #   render "query_parameters.json.jbuilder"
-  # end
 
   def query_params_method
     name = params["name"]
     @output_message = "Your name is #{name}"
+    if params["name"].downcase.starts_with?("a")
+      @output_message = @output_message + " (your name starts with a)"
+    else
+    end
     render "query_parameters.json.jbuilder"
   end
+
+  def guess_query_method
+    winning_number = 32
+    input_guess = params["variable"].to_i
+    if input_guess > winning_number
+      @output_message = "Pick lower!"
+    elsif input_guess < winning_number
+      @output_message = "Pick higher!"
+    else
+      @output_message = "You win!"
+    end
+    render "guess_query.json.jbuilder"  
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/hello_url" => "parameters#hello_method"
   get "/query_params_url" => "parameters#query_params_method"
+  get "/guess_query_url/:variable" => "parameters#guess_query_method"
 end
